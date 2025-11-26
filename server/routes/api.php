fe6cb105f1cdf9826b8fb7cfd3c1497234654e22
novadityap <novaaditya23@gmail.com>
2025-11-26 22:25:19 +0700

    Update composer.lock and api routes: upgrade Cloudinary package, adjust department and employee route middleware for improved authorization

--- server/routes/api.php
+++ server/routes/api.php
@@ -33,27 +33,29 @@ Route::prefix('departments')->middleware('auth:api')->controller(DepartmentContr
     Route::get('/', 'list');
     Route::post('/', 'create');
     Route::get('/search', 'search');
-    Route::get('/{department}', 'show');
     Route::patch('/{department}', 'update');
     Route::delete('/{department}', 'delete');
   });
-});
-
-Route::prefix('employees')->controller(EmployeeController::class)->group(function () {
-  Route::middleware('authorize:employee')->group(function () {
-    Route::post('/', 'create');
-    Route::get('/search', 'search');
-    Route::get('/{employee}', 'show')->middleware('can:show,employee');
-    Route::patch('/{employee}', 'update')->middleware('can:update,employee');
-    Route::delete('/{employee}', 'delete');
-    Route::patch('/{employee}/profile', 'profile')->middleware('can:profile,employee');
+  Route::middleware('authorize:admin,employee')->group(function () {
+    Route::get('/{department}', 'show');
   });
 });
 
+Route::prefix('employees')->middleware(['auth:api', 'authorize:admin'])->controller(EmployeeController::class)->group(function () {
+    Route::post('/', 'create');
+    Route::get('/search', 'search');
+    Route::get('/{employee}', 'show');
+    Route::patch('/{employee}', 'update');
+    Route::delete('/{employee}', 'delete');
+});
+
 Route::prefix('attendances')->middleware('auth:api')->controller(AttendanceController::class)->group(function () {
-  Route::get('/search', 'search')->middleware('authorize:admin');
-  Route::post('/clockIn', 'clockIn');
-  Route::patch('/clockOut', 'clockOut');
+  Route::middleware('authorize:admin')->group(function () {
+    Route::get('/search', 'search');
+    Route::delete('/{attendance}', 'delete');
+  });
+  Route::post('/checkIn', 'checkIn');
+  Route::patch('/checkOut', 'checkOut');
 });
 
 Route::prefix('dashboard')->middleware('auth:api')->controller(DashboardController::class)->group(function () {
